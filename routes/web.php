7310b2ee4623423667d46ced3c92c4b1208d2d06
fendikreasitech <fendi.kreasitech@gmail.com>
2025-08-22 09:43:27 +0700

    feat: Add fallback route for Vue Router in web.php

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+// ===== Tambahkan fallback untuk Vue Router =====
+Route::get('/{any}', function () {
     return view('welcome');
-});
+})->where('any', '.*');
